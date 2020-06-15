bd12eecdc0695d75b172b23059c9f6f35024a25e
yjie28 <xieyy28@gmail.com>
2020-06-14 17:56:53 -0700

    minor changes

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -30,7 +30,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
         dispatch(toggleCartHidden());
       }}
     >
-      GO TO CHECKOUT
+      CHECKOUT
     </CustomButton>
   </div>
 );
